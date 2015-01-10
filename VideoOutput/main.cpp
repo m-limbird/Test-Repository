4927f4cb060a3c46db45c76f7a3feddb906ff772
unknown <Limbird@ROV2875.rovisys.com>
2015-01-10 14:39:15 -0500

    Made significant changes to VideoOutput - still have problems with getting good data, but getting something.

--- VideoOutput/main.cpp
+++ VideoOutput/main.cpp
@@ -1,10 +1,10 @@
 #include "mbed.h"
 
 //Accelerometer
-#define  ADXL345_ADDRESS (0xA6 >> 1)
+#define ADXL345_ADDRESS (0xA6 >> 1) //I2C uses 7 bits, must shift it by 1 to use
 #define ADXL345_REGISTER_XLSB (0x32)
 #define ADXL_REGISTER_PWRCTL (0x2D)
-#define ADXL_PWRCTL_MEASURE (1 << 3)
+#define ADXL_PWRCTL_MEASURE (0x01 << 3)
 
 //Gyroscope
 #define ITG3200_ADDRESS (0xD0 >> 1)
@@ -19,37 +19,33 @@
 #define HMC5843_REGISTER_MEASMODE (0x02)
 #define HMC5843_MEASMODE_CONT (0x00)
 
-//I2C		i2c( p9, p10 );	// sda, scl
+I2C	i2c( p9, p10 );	// sda, scl
 
-//void init_adxl345();
+void init_adxl345();
+void read_adxl345();
 //void init_itg3200();
 //void init_hmc5843();
 
-const int addr = 0x90;
 Serial pc(USBTX, USBRX); //tx, rx
 
-//int adxl345_Data[3];
-//int itg3200_Data[3];
-//int hmc5843_Data[3];
+int accelerometer_data[3];
+//int gyro_data[3];
+//int magnetometer_data[3];
 
 PwmOut led(LED1);
 
 float brightness = 0.0;
 
 int main() {
-	//char cmd[2];
 
-	//init_adxl345();
+	init_adxl345();
 	//init_hmc5843();
 	//init_itg3200();
     
-    pc.printf("Press 'u' to turn LED1 brightness up, 'd' to turn it down.\n");
+    //pc.printf("Press 'u' to turn LED1 brightness up, 'd' to turn it down.\n");
     
-    while(1) {
-        //cmd[0] = 0x01;
-        //cmd[1] = 0x00;
-        //i2c.write(addr, cmd, 2); // i2c.write(address, command, bytes);
-        
+    while(1) {        
+        /*
         char c = pc.getc();
         if((c == 'u') && (brightness < 0.5)) {
         	brightness += 0.01;
@@ -59,32 +55,42 @@ int main() {
         	brightness -= 0.01;
         	led = brightness;
         }
+        */
+        read_adxl345();
         
     }
 }
 
-
-/*
 void init_adxl345() {
-	char data = 0;
+	char data[2];
+	data[0] = ADXL_REGISTER_PWRCTL;
+	data[1] = ADXL_PWRCTL_MEASURE;
 
-	i2c_write(ADXL345_ADDRESS, ADXL_REGISTER_PWRCTL, ADXL_PWRCTL_MEASURE);
+	i2c.write(ADXL345_ADDRESS, data, 2); // first part of data is the register
+	
+	wait(0.5);
 
-	i2c_read(ADXL345_ADDRESS, ADXL_REGISTER_PWRCTL, 1, &data);
-	Serial.println((unsigned int)data);
+	i2c.write(ADXL345_ADDRESS, data, 1);
+	i2c.read(ADXL345_ADDRESS, data, 2);
+	pc.printf("%i\n",(unsigned int)data);
 }
 
 void read_adxl345() {
-	bytes bytes[6];
+	char bytes[6];
 	memset(bytes,0,6);
-
-	i2c_read(ADXL345_ADDRESS, ADXL345_REGISTER_XLSB, 6, bytes);
+	bytes[0] = ADXL345_REGISTER_XLSB;
+	
+	i2c.write(ADXL345_ADDRESS, bytes, 1);
+	i2c.read(ADXL345_ADDRESS, bytes, 6);
 
 	for (int i=0;i<3;++i) {
 		accelerometer_data[i] = (int)bytes[2*i] + (((int)bytes[2*i + 1]) << 8);
  	}
+ 	
+ 	pc.printf("ACCEL: %i\t%i\t%i", accelerometer_data[0], accelerometer_data[1], accelerometer_data[2]);
 }
 
+/*
 void init_itg3200() {
 	byte data = 0;
 
